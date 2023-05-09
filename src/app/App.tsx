e9fd7eabb6784fa8e4403f28a056a74fea3ef41e
EvelinSu <eveelinsu@gmail.com>
2023-05-09 20:59:13 +0500

    add route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { Layout } from 'entities/Layout';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { StyleProvider } from 'app/providers/StyleProvider';
+import { AppRouter } from 'app/providers/router/ui/AppRouter';
 
 export const App = () => {
 
@@ -12,12 +11,7 @@ export const App = () => {
             <Layout>
                 <Link to={ '/' }>Main page</Link>
                 <Link to={ '/about' }>About page</Link>
-                <Suspense fallback={ <div>Loading...</div> }>
-                    <Routes>
-                        <Route path={ '/about' } element={ <AboutPage /> } />
-                        <Route path={ '' } element={ <MainPage /> } />
-                    </Routes>
-                </Suspense>
+                <AppRouter />
             </Layout>
         </StyleProvider>
     );
